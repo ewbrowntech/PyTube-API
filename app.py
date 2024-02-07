44483ff4894d4a042aa6cc8d93771c531d6437ec
Ethan Brown <ethan@ewbrowntech.com>
2024-02-07 14:02:43 -0600

    Initialize FastAPI application

---
+++ app.py
@@ -0,0 +1,27 @@
+"""
+app.py
+
+@Author: Ethan Brown - ethan@ewbrowntech.com
+
+Run FastAPI application
+
+Copyright (C) 2024 by Ethan Brown
+All rights reserved. This file is part of the PyTube-API project and is released under
+the MIT License. See the LICENSE file for more details.
+"""
+
+import os
+import logging
+from fastapi import FastAPI
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+
+from logging import logger
+from limiter import limiter
+
+# Create FastAPI client
+app = FastAPI()
+
+# Configure rate limiting
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
---
+++ logging.py
@@ -0,0 +1,11 @@
+"""
+logging.py
+
+@Author: Ethan Brown - ethan@ewbrowntech.com
+
+Configure logging for application
+
+Copyright (C) 2024 by Ethan Brown
+All rights reserved. This file is part of the PyTube-API project and is released under
+the MIT License. See the LICENSE file for more details.
+"""
